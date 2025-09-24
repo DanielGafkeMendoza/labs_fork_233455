21bfb8863fd67b6b54546367a998e2da843cfd16
DanielGafkeMendoza <233455@students.hertie-school.org>
2025-09-24 09:46:02 +0200

    Lab 3 after class

---
+++ session-03-automation/lotr_project/scripts/tables.R
@@ -0,0 +1,21 @@
+## import clean data
+lotr_dat <- read_tsv("./session-03-automation/lotr_project/data/processed/lotr_clean.tsv") %>% 
+  # reorder Species based on words spoken
+  mutate(Species = reorder(Species, Words, sum))
+
+View(lotr_dat)
+
+# Can you add a new script that creates a summary table of the words spoken by Species
+# and Film and saves it to the `outputs/tables/` folder? You can use the `dplyr` package to
+# create the summary table and the `write_tsv()` function to save it as a .tsv file. 
+# Don't forget to source your new script in the `Makefile.R` script.
+
+words_data <- lotr_dat |>
+  group_by(Film, Species) |>
+  summarize(words_spoken = sum(Words)) |>
+  .groups = drop()
+
+write_tsv(words_data, file = "./session-03-automation/lotr_project/data/processed/words_by_film_and_species.tsv")
+
+View(words_data)
+
